d7ca3150c333b1d3938496ce5bf27f00438beee7
Simon Warta <simon@kullo.net>
2019-03-13 13:24:39 +0100

    Make test "can execute expire" independent of previous test

--- test/swap/AtomicSwapEther.js
+++ test/swap/AtomicSwapEther.js
@@ -1,11 +1,23 @@
+const crypto = require('crypto');
+
 const atomicSwap = artifacts.require("./AtomicSwapEther.sol");
 
+function makeSwapId() {
+  return `0x${crypto.randomBytes(32).toString('hex')}`;
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 contract("Cross Chain Atomic Swap with Ether", (accounts) => {
-  describe("Original suite", () => {
+  const defaultHash = "0x261c74f7dd1ed6a069e18375ab2bee9afcb1095613f53b07de11829ac66cdfcc";
+  const defaultTimeout = 100; // seconds
+
+  describe("Unsorted", () => {
     const lock = "0x261c74f7dd1ed6a069e18375ab2bee9afcb1095613f53b07de11829ac66cdfcc";
     const key = "0x42a990655bffe188c9823a2f914641a32dcbb1b28e8586bd29af291db7dcd4e8";
     const swapID_swap = "0x0505915948dcd6756a8f5169e9c539b69d87d9a4b8f57cbb40867d9f91790211";
-    const swapID_expiry = "0xc3b89738306a66a399755e8535300c42b1423cac321938e7fe30b252abf8fe74";
 
     it("Deposit ether into the contract", async () => {
       const swap = await atomicSwap.deployed();
@@ -35,20 +47,8 @@ contract("Cross Chain Atomic Swap with Ether", (accounts) => {
 
     it("Deposit ether into the contract", async () => {
       const swap = await atomicSwap.deployed();
-      const timeout = 2; // seconds
-      await swap.open(swapID_expiry, accounts[0], lock, timeout, { from: accounts[0], value: 50000 });
-    });
-
-    it("Withdraw after expiry", async () => {
-      await new Promise((resolve, reject) => setTimeout(async () => {
-        try {
-          const swap = await atomicSwap.deployed();
-          await swap.expire(swapID_expiry, { from: accounts[0] });
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      }, 2 * 1000));
+      const swapId = makeSwapId();
+      await swap.open(swapId, accounts[0], lock, defaultTimeout, { from: accounts[0], value: 50000 });
     });
   });
 
@@ -127,6 +127,18 @@ contract("Cross Chain Atomic Swap with Ether", (accounts) => {
       });
 
       assert.ok(await swap.close(swapID_swap, key));
+    });
+  });
+
+  describe("expire()", () => {
+    it("can execute expire", async () => {
+      const swap = await atomicSwap.deployed();
+
+      const swapId = makeSwapId();
+      const timeout = 2;
+      await swap.open(swapId, accounts[0], defaultHash, timeout, { from: accounts[0], value: 50000 });
+      await sleep(2000);
+      await swap.expire(swapId, { from: accounts[0] });
     });
   });
 });
