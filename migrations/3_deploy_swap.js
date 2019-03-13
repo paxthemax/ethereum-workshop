14542b8b0050b973d6f71f66021ca56d426e9b6f
Simon Warta <simon@kullo.net>
2019-03-13 11:28:11 +0100

    Remove Test2ERC20.sol

--- migrations/3_deploy_swap.js
+++ migrations/3_deploy_swap.js
@@ -1,11 +1,9 @@
 const Ether = artifacts.require("./AtomicSwapEther.sol");
 const ERC20 = artifacts.require("./AtomicSwapERC20.sol");
 const TestERC20 = artifacts.require("./TestERC20.sol");
-const Test2ERC20 = artifacts.require("./Test2ERC20.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(Ether);
   deployer.deploy(ERC20);
   deployer.deploy(TestERC20);
-  deployer.deploy(Test2ERC20);
 };
