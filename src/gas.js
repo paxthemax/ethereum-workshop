6692d6e1f734af10b274daeccecad201e9008473
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-03-13 12:04:44 +0100

    Support multiple networks

--- src/gas.js
+++ src/gas.js
@@ -11,7 +11,10 @@ const myFetch = async (url) => {
 const ten8 = new BN(10).pow(new BN(8));
 const toWei = gasRes => new BN(gasRes, 10).mul(ten8).toString();
 
-const getGasPrice = async () => {
+// flatGas returns a fake api response with same gas for all types
+const flatGas = price => new Promise(resolve => resolve({low: price, mid: price, high: price}));
+
+const getMainnetGasPrice = async () => {
   const res = await myFetch(GAS_STATION_URL);
   return {
     low: toWei(res.safeLow),
@@ -20,6 +23,21 @@ const getGasPrice = async () => {
   }
 }
 
+const getGasPriceForNetwork = (network) => {
+  if (network === "mainnet") {
+    return getMainnetGasPrice();
+  }
+  if (["ropsten", "rinkeby", "kovan"].includes(network)) {
+    // 100 GWei
+    return flatGas(toWei("1000"));
+  }
+  if (network === "development") {
+    return flatGas(toWei("20"));
+  }
+  return new Promise((_, reject) => reject(`invalid network: ${network}`));
+}
+
 module.exports = {
-  getGasPrice
+  getMainnetGasPrice,
+  getGasPriceForNetwork,
 }
